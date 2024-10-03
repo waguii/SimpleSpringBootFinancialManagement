c8cd26e0a99bcb1ab2c9a4020b0ada186783bcf1
waguii <wagabsousa@gmail.com>
2024-10-03 19:20:45 -0300

    Initial commit

---
+++ src/main/java/com/uniolab/testefinanceiro/TesteFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.uniolab.testefinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniolab/testefinanceiro/TesteFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniolab.testefinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
